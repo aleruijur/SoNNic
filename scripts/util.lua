42748aeefc77d4ac08dba427095a387dac022df9
makislayer <carlosj93v@gmail.com>
2021-08-16 21:17:26 +0200

    Play and predict now play model from parameter

--- scripts/util.lua
+++ scripts/util.lua
@@ -104,7 +104,7 @@ function util.convertSteerToJoystick(steer, use_mapping)
   -- Ensure that steer is between -1 and 1
   steer = util.clamp(steer, util.STEER_MIN, util.STEER_MAX)
 
-  -- If we are using our mapping, map the linaer steer space to the joystick space.
+  -- If we are using our mapping, map the linear steer space to the joystick space.
   if use_mapping == true then
     steer = util.sign(steer) * math.sqrt(math.abs(steer) * 0.24 + 0.01)
   end
