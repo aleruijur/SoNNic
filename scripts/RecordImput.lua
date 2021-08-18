245c2414c4561625a728e907754a5754440b54c3
makislayer <carlosj93v@gmail.com>
2021-08-18 18:28:34 +0200

    New Script to screenshot only on joystick move

--- scripts/RecordImput.lua
+++ scripts/RecordImput.lua
@@ -1,6 +1,6 @@
 --[[ BEGIN CONFIGURATION ]]--
 RECORD_EVERY = 10 -- Record imput and make a screenshot every # of frames
-PLAY_FOR = 550 -- Target imput and screenshots on this session. Game will end once goal is reached
+PLAY_FOR = 200 -- Target imput and screenshots on this session. Game will end once goal is reached
 USE_MAPPING = true
 --[[ END CONFIGURATION ]]--
 
@@ -18,7 +18,7 @@ os.execute('mkdir ' .. RECORDING_FOLDER)
 os.execute('type nul > ' .. RECORDING_FOLDER .. '\\steering.txt')
 
 client.unpause()
-client.speedmode(75)
+client.speedmode(50)
 
 function onexit()
   if steering_file ~= nil then
@@ -31,18 +31,21 @@ end
 local exit_guid = event.onexit(onexit)
 
 local recording_frame = 1
-if RECORDING_START_FRAME ~= nil then recording_frame = RECORDING_START_FRAME end
 
 local steering_file = io.open(RECORDING_FOLDER .. '\\steering.txt', 'a')
 local action={}
 while recording_frame < PLAY_FOR do
     action=joypad.get()
-    client.screenshot(RECORDING_FOLDER .. '\\' .. recording_frame .. '.png')
-    steering_file:write(action["P1 X Axis"] .. '\n')
-    steering_file:flush()
-    recording_frame = recording_frame + 1
-    for i=0, RECORD_EVERY do emu.frameadvance() end
-
+    -- This script will only screenshot if you're moving the joystick
+    if(action["P1 X Axis"]==0) then
+        emu.frameadvance()
+    else
+        client.screenshot(RECORDING_FOLDER .. '\\' .. recording_frame .. '.png')
+        steering_file:write(action["P1 X Axis"] .. '\n')
+        steering_file:flush()
+        recording_frame = recording_frame + 1
+        for i=0, RECORD_EVERY do emu.frameadvance() end
+    end
 end
 onexit()
 event.unregisterbyid(exit_guid)
