1d707468bef0d264848291a9bdf6cb21b492e7ed
makislayer <carlosj93v@gmail.com>
2021-08-24 20:51:46 +0200

    Demo.lua now plays 4 tracks

--- scripts/Demo.lua
+++ scripts/Demo.lua
@@ -4,8 +4,8 @@ Demo.lua
 This script plays selected tracks forever in a loop, for purposes of demoing the AI. ]]--
 
 --[[ BEGIN CONFIGURATION ]]--
-RACE_END_FRAMES = 700 -- The number of frames to show the time cards at the end of a race.
-TRACKS = {'states/TT/LR.state', 'states/GP/MMF.state', 'states/TT/CM.state'}
+TRACKS = {'../states/MuteCityTA.state', '../states/MuteCityGP2.state', '../states/MuteCity2.state', '../states/MuteCityGP.state'}
+PLAY_FOR_FRAMES = {2300,500,200,800} -- Number of frames that each track is going to play
 --[[ END CONFIGURATION ]]--
 
 local util = require("util")
@@ -17,21 +17,12 @@ event.onexit(function()
 end)
 
 local state = 1
+local frame = 0
 while true do
-    -- Start from the beginning and play.
+    -- Load the savestate from the list
+    print("Loading:", TRACKS[state])
     savestate.load(TRACKS[state])
-    print("Mode:", util.readMode(), "Course:", util.readCourse())
-
-    play()
-
-    -- If the race was successful, play a little but forward so that we can see the times.
-    if util.readProgress() >= 3 then
-      client.unpause()
-      print ("Time (seconds):", util.readTimer())
-      for i=1, RACE_END_FRAMES do emu.frameadvance() end
-    end
-
-    -- Go to the next track.
-    state = state + 1
+    play(PLAY_FOR_FRAMES[state]) -- Play for just some frames on this track
+    state = state + 1    -- Go to the next track.
     if state > #TRACKS then state = 1 end
 end
